c8d3c409ab5e184d5262e40eaa3f7b6a2c82022d
Guilherme <gp.antunes@campus.fct.unl.pt>
2024-12-05 14:30:43 +0000

    adicionei o client pro redis, pode estar mal

---
+++ user_shorts_logic/src/main/java/tukano/clients/RedisClient.java
@@ -0,0 +1,91 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.HashMap;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class RedisRestClient {
+    private final String baseUrl;
+    private final HttpClient httpClient;
+    private final ObjectMapper objectMapper;
+
+    public RedisRestClient(String baseUrl) {
+        this.baseUrl = baseUrl.endsWith("/") ? baseUrl : baseUrl + "/";
+        this.httpClient = HttpClient.newHttpClient();
+        this.objectMapper = new ObjectMapper();
+    }
+
+    public String get(String key) throws Exception {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(baseUrl + "get/" + key))
+                .GET()
+                .build();
+
+        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+
+        if (response.statusCode() == 200) {
+            return response.body();
+        } else {
+            throw new RuntimeException("Failed to get key: " + response.statusCode() + " " + response.body());
+        }
+    }
+
+    public String set(String key, String value) throws Exception {
+        HashMap<String, String> data = new HashMap<>();
+        data.put("key", key);
+        data.put("value", value);
+
+        String requestBody = objectMapper.writeValueAsString(data);
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(baseUrl + "set"))
+                .header("Content-Type", "application/json")
+                .POST(HttpRequest.BodyPublishers.ofString(requestBody))
+                .build();
+
+        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+
+        if (response.statusCode() == 200) {
+            return response.body();
+        } else {
+            throw new RuntimeException("Failed to set key: " + response.statusCode() + " " + response.body());
+        }
+    }
+
+    public String delete(String key) throws Exception {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(baseUrl + "delete/" + key))
+                .DELETE()
+                .build();
+
+        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+
+        if (response.statusCode() == 200) {
+            return response.body();
+        } else {
+            throw new RuntimeException("Failed to delete key: " + response.statusCode() + " " + response.body());
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            RedisRestClient redisClient = new RedisRestClient("http://redis-rest-service:8080/");
+
+            // Set a value
+            String setResponse = redisClient.set("exampleKey", "exampleValue");
+            System.out.println("Set response: " + setResponse);
+
+            // Get the value
+            String value = redisClient.get("exampleKey");
+            System.out.println("Get response: " + value);
+
+            // Delete the key
+            String deleteResponse = redisClient.delete("exampleKey");
+            System.out.println("Delete response: " + deleteResponse);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
